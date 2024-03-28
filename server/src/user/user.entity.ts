06c8bb441c1d657ea67488dfd3ccca084d969d17
Абдурахман Усманов <ausmanov706@gmail.com>
2024-03-28 17:06:11 +0300

    feat: server: user module

---
+++ server/src/user/user.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from 'typeorm';
+import { hash } from 'bcrypt';
+
+@Entity({ name: 'users' })
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column({ default: '' })
+  image: string;
+
+  @Column()
+  password: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await hash(this.password, 5);
+  }
+}
