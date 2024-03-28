580fc8948164cee8b8dd474e0998c04f931d7a78
Абдурахман Усманов <ausmanov706@gmail.com>
2024-03-28 17:48:53 +0300

    fix: server: migrations

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { config } from './ormconfig';
+import { config } from './data-source';
 import { UserModule } from './user/user.module';
 
 @Module({
