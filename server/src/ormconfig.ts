071190b95701d7e3362fa020dcb0e1fba2a608d4
Абдурахман Усманов <ausmanov706@gmail.com>
2024-03-28 16:11:57 +0300

    feat: server: database connection

---
+++ server/src/ormconfig.ts
@@ -0,0 +1,13 @@
+import { DataSourceOptions } from 'typeorm';
+
+export const config: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'fast_notation_user',
+  password: '1234',
+  database: 'fast_notation',
+  entities: [__dirname + '/**/*.entity{.ts, .js}'],
+  synchronize: false,
+  migrations: [__dirname + '/migrations/**/*{.ts, .js}'],
+};
